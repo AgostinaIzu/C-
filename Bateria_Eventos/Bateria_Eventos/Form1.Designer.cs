9deb7190100a3a84e694d496c8c39d88d3f8d3c6
AgostinaIzu <75485913+AgostinaIzu@users.noreply.github.com>
2021-07-28 16:22:04 -0300

    Add files via upload

---
+++ Bateria_Eventos/Bateria_Eventos/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Bateria_Eventos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Boton_OnOff = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Boton_Cargar = new System.Windows.Forms.Button();
+            this.Boton_Usar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Boton_OnOff
+            // 
+            this.Boton_OnOff.Location = new System.Drawing.Point(154, 219);
+            this.Boton_OnOff.Name = "Boton_OnOff";
+            this.Boton_OnOff.Size = new System.Drawing.Size(283, 25);
+            this.Boton_OnOff.TabIndex = 8;
+            this.Boton_OnOff.Text = "Apagar";
+            this.Boton_OnOff.UseVisualStyleBackColor = true;
+            this.Boton_OnOff.Click += new System.EventHandler(this.Boton_OnOff_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(154, 148);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(283, 50);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Boton_Cargar
+            // 
+            this.Boton_Cargar.Location = new System.Drawing.Point(323, 91);
+            this.Boton_Cargar.Name = "Boton_Cargar";
+            this.Boton_Cargar.Size = new System.Drawing.Size(114, 37);
+            this.Boton_Cargar.TabIndex = 6;
+            this.Boton_Cargar.Text = "Cargar";
+            this.Boton_Cargar.UseVisualStyleBackColor = true;
+            this.Boton_Cargar.Click += new System.EventHandler(this.Boton_Cargar_Click);
+            // 
+            // Boton_Usar
+            // 
+            this.Boton_Usar.Location = new System.Drawing.Point(154, 91);
+            this.Boton_Usar.Name = "Boton_Usar";
+            this.Boton_Usar.Size = new System.Drawing.Size(114, 37);
+            this.Boton_Usar.TabIndex = 5;
+            this.Boton_Usar.Text = "Usar";
+            this.Boton_Usar.UseVisualStyleBackColor = true;
+            this.Boton_Usar.Click += new System.EventHandler(this.Boton_Usar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 335);
+            this.Controls.Add(this.Boton_OnOff);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Boton_Cargar);
+            this.Controls.Add(this.Boton_Usar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Boton_OnOff;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button Boton_Cargar;
+        private System.Windows.Forms.Button Boton_Usar;
+    }
+}
+
---
+++ Bateria_Eventos/Bateria_Eventos/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bateria_Eventos
+{
+    public partial class Form1 : Form
+    {
+        Bateria B;
+        Cargador C;
+
+        public Form1()
+        {
+            InitializeComponent();
+            B = new Bateria();
+            C = new Cargador();
+
+            progressBar1.Value = B.Nivel_de_Carga;
+        }
+
+        private void Boton_Usar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                B.Bateria_baja += FuncionAlertaCarga; //Suscripciones a eventos
+                B.Bateria_agotada += FuncionApagado;
+
+                B.Nivel_de_Carga -= 10;
+                progressBar1.Value = B.Nivel_de_Carga;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                B.Bateria_baja -= FuncionAlertaCarga; //Desafecciones a eventos
+                B.Bateria_agotada += FuncionApagado;
+            }
+        }
+
+        private void Boton_Cargar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                C.Cargar(B);
+                progressBar1.Value = B.Nivel_de_Carga;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Boton_OnOff_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Boton_Usar.Enabled == false) { FuncionEncendido(this, null); }
+                else { FuncionApagado(this, null); }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void FuncionAlertaCarga(object sender, Bateria_bajaEvenArgs e) //Metodo evento Bateria_baja
+        {
+            MessageBox.Show("Nivel de bateria: " + e.NivelBateria + "% \n Bateria baja, cargue su dispositivo");
+        }
+        private void FuncionApagado(object sender, EventArgs e) //Metodo evento Bateria_agotada
+        {
+            Boton_Usar.Enabled = false;
+            Boton_OnOff.Text = "Encender";
+        }
+        private void FuncionEncendido(object sender, EventArgs e)
+        {
+            if (B.Nivel_de_Carga > 0)
+            {
+                Boton_Usar.Enabled = true;
+                Boton_OnOff.Text = "Apagar";
+            }
+            else { MessageBox.Show("Cargue su dispositivo"); }
+        }
+    }
+
+    public class Bateria
+    {
+        int Carga;
+
+        public event EventHandler<Bateria_bajaEvenArgs> Bateria_baja;
+        public event EventHandler Bateria_agotada;
+
+        public int Nivel_de_Carga
+        {
+            get => Carga;
+            set
+            {
+                Carga = value;
+                if (value <= 10)
+                {
+                    if (value == 10)
+                    {
+                        //DESENCADENAMIENTO DEL EVENTO
+                        Bateria_baja?.Invoke(this, new Bateria_bajaEvenArgs(value));
+                    }
+                    else { Bateria_agotada?.Invoke(this, null); }
+                }
+            }
+        }
+        public Bateria() { Nivel_de_Carga = 50; }
+    }
+    public class Bateria_bajaEvenArgs : EventArgs
+    {
+        int B;
+        public int NivelBateria { get => B; }
+
+        public Bateria_bajaEvenArgs(int nivelbateria) //Constructor
+        {
+            B = nivelbateria;
+        }
+    }
+    public class Cargador
+    {
+        public void Cargar(Bateria B)
+        {
+            if (B.Nivel_de_Carga < 100) { B.Nivel_de_Carga = 100; }
+            else { MessageBox.Show("Bateria cargada. Desconecte el cargador"); }
+        }
+    }
+}
